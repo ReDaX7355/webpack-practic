5cf8f7309fed731799972fcecf6ad59fc5873a99
ReDaX7355 <43163998+ReDaX7355@users.noreply.github.com>
2023-09-08 10:06:41 +0300

    5 files modified, 5 added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import Notes from './notes.csv';
+import data from './data.xml';
+
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
   const element = document.createElement('div');
@@ -13,6 +28,9 @@ function component() {
   myImg.src = Icon;
 
   element.appendChild(myImg);
+
+  console.log(Notes);
+  console.log(data);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const yaml = require('yaml');
+const toml = require('toml');
+const json5 = require('json5');
 
 module.exports = {
   entry: './src/index.js',
@@ -19,6 +22,35 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        },
       },
     ],
   },
