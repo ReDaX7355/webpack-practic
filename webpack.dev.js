107648587eabd3225976328a8c7ba404ea87a756
ReDaX7355 <43163998+ReDaX7355@users.noreply.github.com>
2023-09-15 16:07:11 +0300

    6 files modified, 4 added, 3 deleted

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,19 @@
 const { merge } = require('webpack-merge');
 const path = require('path');
 const webpackConfig = require('./webpack.config');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+plugins = [
+  new MiniCssExtractPlugin({
+    filename: '[name].[contenthash].css',
+  }),
+];
 
 module.exports = merge(webpackConfig, {
   mode: 'development',
-
+  target: 'web',
+  plugins,
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].[contenthash].js',
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,38 @@
 const { merge } = require('webpack-merge');
 const path = require('path');
 const webpackConfig = require('./webpack.config');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+plugins = [
+  new MiniCssExtractPlugin({
+    filename: '[contenthash].css',
+  }),
+];
 
 module.exports = merge(webpackConfig, {
   mode: 'production',
+  target: 'browserslist',
+  plugins,
+  devtool: false,
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
+    filename: '[fullhash].js',
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          mangle: false,
+          compress: true,
+          output: {
+            beautify: true,
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
   },
 });
