4a7246681f3d22dbf864bced5e657807d6ab5199
ReDaX7355 <43163998+ReDaX7355@users.noreply.github.com>
2023-09-29 13:46:41 +0300

    4 files modified, 3 added, 2 deleted

--- src/Data.tsx
+++ src/Data.tsx
@@ -2,14 +2,44 @@ import React, { useEffect, useState } from 'react';
 import { getRequestsByKey } from './api/requests';
 import { Link } from 'react-router-dom';
 
-const Data = () => {
-  const [requests, setRequests] = useState([]);
+import axios from 'axios';
 
-  useEffect(() => {
-    fetch('http://localhost:3000/requests?_embed=massages&_expand=user')
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
+const Data = () => {
+  const [titleRequest, setTitleRequest] = useState('');
+  const [descriptionRequest, setDescriptionRequest] = useState('');
+  const [typeRequest, setTypeRequest] = useState('incident');
+  const [priorityRequest, setPriorityRequest] = useState('');
+
+  useEffect(() => {}, []);
+
+  const onSubmit = (e: Event) => {
+    e.preventDefault();
+
+    let request;
+
+    axios
+      .get('http://localhost:3000/requests?_sort=id&_order=desc&_limit=1')
+      .then((res) => 
+
+      request = {
+        id: res.data[0].id + 1,
+        title: titleRequest,
+        description: descriptionRequest,
+        applicant: "",
+        create_date: new Date().toLocaleString('ru-RU').replace(',', ''),
+        completed_date: '',
+        type_request: typeRequest,
+        userId: 0,
+        priority: priorityRequest,
+        completed: false,
+    }).then(res => axios.post("http://localhost:3000/requests", res));
+
+
+
+    console.log(request);
+
+    // axios.post("http://localhost:3000/requests", request)
+  };
 
   return (
     <div>
@@ -17,12 +47,36 @@ const Data = () => {
       <Link to="/">App</Link>
       <hr />
       <div>
-        {requests.map((item) => (
-          <div key={item.id}>
-            <h2>{item.title}</h2>
-            <div>{item.create_date}</div>
-          </div>
-        ))}
+        <h3>Подача заявки</h3>
+        <form action="" onSubmit={(e) => onSubmit(e)} className="flex flex-col">
+          <input
+            type="text"
+            placeholder="Тема"
+            onChange={(e) => setTitleRequest(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Описание проблемы"
+            onChange={(e) => setDescriptionRequest(e.target.value)}
+          />
+          <select
+            name="type_request"
+            id="type_request"
+            onChange={(e) => setTypeRequest(e.target.value)}
+          >
+            <option value="incident">Инцидент</option>
+            <option value="question">Вопрос</option>
+          </select>
+          <select
+            name="priority"
+            id="priority"
+            onChange={(e) => setPriorityRequest(e.target.value)}
+          >
+            <option value="">Обычный</option>
+            <option value="high">Высокий</option>
+          </select>
+          <input type="submit" value="Отпрвить" />
+        </form>
       </div>
     </div>
   );
