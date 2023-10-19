5a809c500452a6e87428dc9ffc5b835b5a969d14
ReDaX7355 <43163998+ReDaX7355@users.noreply.github.com>
2023-10-19 15:15:48 +0300

    5 files modified, 2 added, 2 deleted

--- src/Tickets.tsx
+++ src/Tickets.tsx
@@ -1,48 +1,61 @@
 import axios from 'axios';
 import React, { FC, useEffect, useState } from 'react';
 
-const Tickets:FC = () => {
-
+const Tickets: FC = () => {
   const [tickets, setTickets] = useState([]);
 
   useEffect(() => {
+    axios
+      .get('http://localhost:3000/requests')
+      .then((res) => {
+        setTickets(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    axios.get('http://localhost:3000/requests').then(
-      (res) => {
-        
-        setTickets(res.data)
-      }
-    ).catch((err) => console.log(err))
+  let TABLE_COLUMNS = [
+    {
+      label: 'id',
+      sort: 'default',
+    },
+    {
+      label: 'title',
+      sort: 'default',
+    },
+    {
+      label: 'status',
+      sort: 'default',
+    },
+  ];
 
-  }, [])
-
+  const sortTickets = () => {
+    setTickets((prev) => [...prev.sort((a, b) => b.id - a.id)]);
+    console.log(tickets);
+  };
 
   return (
     <div>
       <h3>Tickets</h3>
 
-      <table>
+      <table className="table-tickets">
         <thead>
           <tr>
-            <th>Номер заявки</th>
+            <th onClick={() => sortTickets()}>Номер заявки</th>
             <th>Тема</th>
             <th>Дата создания</th>
-            <th>Статус</th>
+            <th onClick={() => sortTickets()}>Статус</th>
           </tr>
         </thead>
         <tbody>
-        {tickets && 
-          tickets.map(ticket => 
+          {tickets.map((ticket) => (
             <tr>
               <td>{ticket.id}</td>
               <td>{ticket.title}</td>
               <td>{ticket.created_at}</td>
-              <td>{ticket.completed ? "Закрыта": "Открыта"}</td>
+              <td>{ticket.completed ? 'Закрыта' : 'Открыта'}</td>
             </tr>
-          )
-        }
+          ))}
         </tbody>
-        
       </table>
     </div>
   );
