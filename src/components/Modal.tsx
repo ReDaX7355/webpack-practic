5c96a2be752e10d4001c7113588cc116716834c5
ReDaX7355 <43163998+ReDaX7355@users.noreply.github.com>
2023-11-29 09:29:14 +0300

    1 file modified

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,26 +1,29 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC, ReactNode, useEffect, useState } from 'react';
 
 interface ModalProps {
-  isActive: boolean;
+  isActive?: boolean;
   onClose: () => void;
   children: ReactNode;
   top?: number;
   left?: number;
 }
 
-const Modal: FC<ModalProps> = ({
-  isActive,
-  onClose,
-  children,
-  top = 8,
-  left = 8,
-}) => {
-  const styles = `block absolute top-${top} left-${left} min-w-[150px] bg-white shadow p-4 rounded z-20`;
+const Modal: FC<ModalProps> = ({ onClose, children, top = 8, left = 8 }) => {
+  const styles = ` block absolute top-${top} left-${left} min-w-[150px] bg-white shadow p-4 rounded z-20`;
+  const [isActive, setIsActive] = useState(false);
+  useEffect(() => {
+    window.addEventListener('click', () => {
+      let modalHeader = document.querySelector('.modal-header');
+
+      if (modalHeader?.closest('.modal-header')) {
+      }
+    });
+  }, []);
 
   if (!isActive) return null;
   return (
-    <div>
-      <div className={isActive ? 'back-active' : ''} onClick={onClose}></div>
+    <div className="modal">
+      <div onClick={onClose}></div>
       <div className={styles}>{children}</div>
     </div>
   );
