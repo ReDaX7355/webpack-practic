6f6aa2c8c40f72d2253b3198d00c31bc9a951990
ReDaX7355 <43163998+ReDaX7355@users.noreply.github.com>
2023-11-30 11:59:23 +0300

    2 files modified

--- src/Root.tsx
+++ src/Root.tsx
@@ -10,7 +10,7 @@ const Root = () => {
       <div className="bg-gray-100 h-[100%]">
         <h1 className="container mx-auto px-3 py-6 text-3xl font-bold">Root</h1>
         <App />
-        <div className="container py-5 mx-auto">
+        <div className="container py-10 mx-auto">
           <Outlet />
         </div>
       </div>
--- src/TicketPage.tsx
+++ src/TicketPage.tsx
@@ -6,7 +6,7 @@ const TicketPage: FC = () => {
   const [ticket, setTicket] = useState<ITicket | []>([]);
 
   let { ticket_number } = useParams();
-  
+
   useEffect(() => {
     localStorage.setItem('currentTicketId', ticket_number || '');
     getTicketsByKey('ticket_number', ticket_number).then((res) => {
@@ -30,10 +30,66 @@ const TicketPage: FC = () => {
   }, []);
 
   return (
-    <div className="container mx-auto">
-      <p className="text-xl text-bold">Заявка номер: {ticket.ticket_number}</p>
-      <div>
-        <span>{ticket.created_at}</span>
+    <div className="container mx-auto bg-white h-[700px] rounded p-7">
+      <div className="flex">
+        <div className="grow">
+          <h1 className="text-2xl font-bold">Данные заявки</h1>
+          <div className="mt-10">
+            <ul className="flex flex-col gap-5">
+              <li className="text-primary">
+                <p className="text-sm">Номер заявки: </p>
+                <p className="font-medium">{ticket.ticket_number}</p>
+              </li>
+              <li>
+                <p className="text-gray-500 text-sm">Тема: </p>
+                <p className="font-medium">{ticket.title}</p>
+              </li>
+              <li>
+                <p className="text-gray-500 text-sm">Создана: </p>
+                <p className="font-medium">{ticket.created_at}</p>
+              </li>
+              <li>
+                <p className="text-gray-500 text-sm">Заявитель: </p>
+                <p className="font-medium">{ticket.applicant_name}</p>
+              </li>
+              <li>
+                <p className="text-gray-500 text-sm">Тип заявки: </p>
+                <p className="font-medium">
+                  {ticket.type_request === 'incident' ? 'Инцидент' : 'Вопрос'}
+                </p>
+              </li>
+              <li>
+                <p className="text-gray-500 text-sm">Приоритет: </p>
+                <p className="font-medium">
+                  {ticket.priority === '' ? 'Обычный' : 'Высокий'}
+                </p>
+              </li>
+              <li>
+                <p className="text-gray-500 text-sm">Исполнитель: </p>
+                <p className="font-medium">
+                  {ticket.assigned_to ? ticket.assigned_to : 'Не назначен'}
+                </p>
+              </li>
+              <li>
+                <p className="text-gray-500 text-sm">Статус заявки: </p>
+                <p className="font-medium">
+                  {ticket.completed
+                    ? 'Закрыта'
+                    : ticket.assigned_to
+                    ? 'В работе'
+                    : 'Открыта'}
+                </p>
+              </li>
+              {ticket.closed_at && (
+                <li>
+                  <p className="text-gray-500 text-sm">Дата закрытия: </p>
+                  <p className="font-medium">{ticket.closed_at}</p>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+        <div className="grow"></div>
       </div>
     </div>
   );
